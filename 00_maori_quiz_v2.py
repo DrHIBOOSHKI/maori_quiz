1c9706f060f14eeb2b6010e2f0671f96ff2f5622
DrHIBOOSHKI <jirehpearson@outlook.com>
2023-05-11 11:38:00 +1200

    update

--- 00_maori_quiz_v2.py
+++ 00_maori_quiz_v2.py
@@ -25,38 +25,98 @@ elif age > 11:
 print("Question 1")
 answer_1 = input("What is the Maori word for 'Family'? ")
 if answer_1 == "Whanau".lower():
-    print("Correct")
+    print(formatter("*", "Correct"))
+            print()
 else:
-    print("Incorrect")
+    print(formatter("!", "Incorrect"))
+            print()
 
 
 print("Question 2")
-answer_2 = input("What is the Maori word for 'Wha'? ")
-if answer_2 == "Four".lower():
-    print("Correct")
+answer_2 = input("What is the Maori word for 'Four'? ")
+if answer_2 == "Wha".lower():
+    print(formatter("*", "Correct"))
+    print()
 else:
-    print("Incorrect")
+    print(formatter("!", "Incorrect"))
+    print()
 
 
 print("Question 3")
-answer_3 = input("What is the Maori word for 'Waru'? ")
-if answer_3 == "Eight".lower():
-    print("Correct")
+answer_3 = input("What is the Maori word for 'Eight'? ")
+if answer_3 == "Waru".lower():
+    print(formatter("*", "Correct"))
+    print()
 else:
-    print("Incorrect")
+    print(formatter("!", "Incorrect"))
+    print()
 
 
 print("Question 4")
-answer_1 = input("What is the Maori word for 'Tekau'? ")
-if answer_1 == "Ten".lower():
-    print("Correct")
+answer_4 = input("What is the Maori word for 'Ten'? ")
+if answer_4 == "Tekau".lower():
+    print(formatter("*", "Correct"))
+    print()
 else:
-    print("Incorrect")
+    print(formatter("!", "Incorrect"))
+    print()
 
 
 print("Question 5")
-answer_1 = input("What is the Maori word for 'Family'?")
-if answer_1 == "Whanau".lower():
-    print("Correct")
+answer_5 = input("What is the Maori word for 'May'? ")
+if answer_5 == "Haratua".lower():
+    print(formatter("*", "Correct"))
+    print()
 else:
-    print("Incorrect")
+    print(formatter("!", "Incorrect"))
+    print()
+
+
+print("Question 6")
+answer_6 = input("What is the Maori word for 'June'? ")
+if answer_6 == "Pipiri".lower():
+    print(formatter("*", "Correct"))
+    print()
+else:
+    print(formatter("!", "Incorrect"))
+    print()
+
+
+print("Question 7")
+answer_7 = input("What is the Maori word for 'September'? ")
+if answer_7 == "Hepetema".lower():
+    print(formatter("*", "Correct"))
+    print()
+else:
+    print(formatter("!", "Incorrect"))
+    print()
+
+
+print("Question 8")
+answer_8 = input("What is the Maori word for 'Monday'? ")
+if answer_8 == "Rahina".lower():
+    print(formatter("*", "Correct"))
+    print()
+else:
+    print(formatter("!", "Incorrect"))
+    print()
+
+
+print("Question 9")
+answer_9 = input("What is the Maori word for 'Wednesday'? ")
+if answer_9 == "Raapa".lower():
+    print(formatter("*", "Correct"))
+    print()
+else:
+    print(formatter("!", "Incorrect"))
+    print()
+
+
+print("Question 10")
+answer_10 = input("What is the Maori word for 'Saturday'? ")
+if answer_10 == "Rahoroi".lower():
+    print(formatter("*", "Correct"))
+    print()
+else:
+    print(formatter("!", "Incorrect"))
+    print()
