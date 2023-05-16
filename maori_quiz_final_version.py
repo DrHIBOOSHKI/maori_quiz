d8ebaf24577766408fd4207a8141466fda94bd34
DrHIBOOSHKI <jirehpearson@outlook.com>
2023-05-17 11:19:14 +1200

    Update maori_quiz_final_version.py

--- maori_quiz_final_version.py
+++ maori_quiz_final_version.py
@@ -11,7 +11,6 @@ def get_age():
 
 # function to format text output
 def formatter(symbol, text):
-    
     sides = symbol * 3
     formatted_text = f"{sides} {text} {sides}"
     top_bottom = symbol * len(formatted_text)
